ce3c3c2623fa43d5302e10264fbcf409f32815f6
Toshi E <anch.abc.t@gmail.com>
2023-07-24 07:20:11 +0900

    Add filtering expense list function

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,7 +38,7 @@ const ExpenseForm = (props) => {
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: new DataTransfer(enteredDate)
+            date: new Date(enteredDate)
         };
 
         props.onSaveExpenseData(expenseData);
