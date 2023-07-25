c11b804762442aba3d998895a583096f5558d0f0
Toshi E <anch.abc.t@gmail.com>
2023-07-25 15:16:33 +0900

    Finish dev

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -37,11 +37,14 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         };
 
+        // Pass data to the parent
         props.onSaveExpenseData(expenseData);
+
+        // Clear data
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
@@ -80,6 +83,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancel}>Cancel</button>
                 <button type='submit'>Add Expense</button>
             </div>
         </form>
